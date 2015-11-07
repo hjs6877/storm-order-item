5948ca8abde0a2e1661806fc4910d3aee718a946
hjs6877 <hjs6877@gmail.com>
2015-11-07 23:47:55 +0900

    구현중..

--- order-item/src/main/java/kr/ac/korea/LineItemEmitter.java
+++ order-item/src/main/java/kr/ac/korea/LineItemEmitter.java
@@ -1,10 +1,12 @@
 package kr.ac.korea;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import storm.trident.operation.TridentCollector;
 import storm.trident.spout.ITridentSpout;
 import storm.trident.topology.TransactionAttempt;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.List;
@@ -12,14 +14,54 @@ import java.util.List;
  * Created by ideapad on 2015-11-05.
  */
 public class LineItemEmitter implements ITridentSpout.Emitter<Long>, Serializable {
+    private static final Logger logger = LoggerFactory.getLogger(LineItemEmitter.class);
     private static final long serialVersionUID = 1L;
     AtomicInteger successfulTransactions = new AtomicInteger(0);
-
+    private FileReader reader;
+    private BufferedReader br;
     @Override
     public void emitBatch(TransactionAttempt transactionAttempt, Long aLong, TridentCollector collector) {
         /**
          * LineItem 데이터를 파일에서 읽어와서 계속 내보낸다.
          */
+        try {
+            reader =   new FileReader("D:\\7_project\\1_private\\2_korea\\data\\lineitem.smaill.tbl");
+            br = new BufferedReader(reader);
+
+            String line = "";
+
+            boolean isContinue = true;
+            while(isContinue){
+
+                line = br.readLine();
+
+                if(line == null){
+                    br.reset();
+                }else {
+                    logger.info("#### input sentence:: " + line);
+                    List<Object> lineItemList = new ArrayList<Object>();
+
+                    LineItem lineItem = new LineItem(1, 15000.0, 0.04);
+                    lineItemList.add(lineItem);
+                    collector.emit(lineItemList);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            logger.info("marked exception");
+//            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
         for(int i = 0; i < 10000; i++){
             List<Object> lineItemList = new ArrayList<Object>();
 
--- order-item/src/main/java/kr/ac/korea/OrderItemTopology.java
+++ order-item/src/main/java/kr/ac/korea/OrderItemTopology.java
@@ -8,7 +8,12 @@ import storm.trident.Stream;
 import storm.trident.TridentTopology;
 
 /**
- * Created by ideapad on 2015-11-05.
+ * 1. LineItemEmitter에서 LineItem 데이터를 LineItemFilter로 내보낸다.
+ *      1.1. LineItemFilter에서 필터링 된 LineItem 데이터를 JoinFunction으로 내보낸다.
+ * 2. OrderEmitter에서 Order 데이터를 JoinFunction으로 내보낸다.
+ * 3. JoinFunction에서 Order와 LineItem을 Join하여 xxxFunction으로 내보낸다.
+ * 4. xxxFunction에서 들어온 Order-LineItem Join 데이터를 맵에 저장한다.
+ * 5. 데이터를 출력한다.
  */
 public class OrderItemTopology {
     public static StormTopology buildTopology() {
